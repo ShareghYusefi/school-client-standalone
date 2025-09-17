8c86dfcb877964abe8290cde10263d0958627ccb
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-09-16 19:21:21 -0600

    dynamic isLoggedIn variable

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar implements OnInit {
+  isLoggedIn: boolean = false;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    // subscribe to the isLoggedIn$ observable
+    this.authService.isLoggedIn$.subscribe((isLoggedIn) => {
+      this.isLoggedIn = isLoggedIn;
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.isLoggedIn = false;
+    // Optionally, navigate to login or home page
+    this.router.navigate(['/login']);
+  }
+}
--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -10,13 +10,40 @@ export class AuthService {
   private apiUrl = environment.apiUrl; // localhost:3000
   private tokenKey = 'jwt_token';
 
+  // function for tracking login state via token
+  private isLoggedIn(): boolean {
+    // check if token is set
+    if (!this.tokenKey) {
+      return false;
+    }
+    // alternatively we could decode the token
+    return true;
+  }
+
+  // BehaviorSubject tracks the returned state from isLoggedIn()
+  private isLoggedInSubject = new BehaviorSubject<boolean>(this.isLoggedIn());
+  public isLoggedIn$ = this.isLoggedInSubject.asObservable();
+
   constructor(private http: HttpClient) {}
 
   login(email: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/login`, {
-      email, // shorthand for email: email
-      password, // shorthand for password: password
-    });
+    return (
+      this.http
+        .post<any>(`${this.apiUrl}/login`, {
+          email, // shorthand for email: email
+          password, // shorthand for password: password
+        })
+        // pipe is used to handle response
+        .pipe(
+          // tap allows us to perform a side effect like updating the login state
+          tap((response: { message: string; token: string }) => {
+            if (response && response.token) {
+              // update login state
+              this.isLoggedInSubject.next(true);
+            }
+          })
+        )
+    );
   }
 
   register(email: string, password: string): Observable<any> {
